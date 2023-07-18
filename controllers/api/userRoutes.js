f40c087465f4a5dc9bff40ecd68737de3f120661
kevhuff <kevinhuffman23@gmail.com>
2023-07-17 21:26:41 -0400

    added api folder, user routes and modified user.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        // Set up sessions with a 'loggedIn' variable set to `true`
+        req.session.save(() => {
+        req.session.loggedIn = true;
+        res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    });
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+        res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again' });
+        return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+        res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again' });
+        return;
+        }
+        // Set up sessions with a 'loggedIn' variable set to `true`
+        req.session.save(() => {
+        req.session.loggedIn = true;
+        res
+            .status(200)
+            .json({ user: userData, message: 'You are now logged in!' });
+        });}
+    catch (err) {
+        res.status(400).json(err);
+    }
+    });
+    router.post('/logout', (req, res) => {
+        if (req.session.loggedIn) {
+            // Remove the session variables
+            req.session.destroy(() => {
+                res.status(204).end();
+            });
+        }
+    });
+    
+    module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,116 +1,71 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+module.exports = function (sequelize, DataTypes) {
+  class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
   }
-}
 
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
       },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8, 20],
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-    },
-  },
-  
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
       },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
-  );
-  
-  module.exports = User;
-
-
-
-
-  const bcrypt = require('bcrypt');
-  const saltRounds = 10; // 10 salt rounds for hashing password 
-  
-  module.exports = function (sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-  
-  
       password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: [8, 20],
+        },
         set(val) {
-          const hash = bcrypt.hashSync(val, saltRounds);
+          const hash = bcrypt.hashSync(val, 10);
           this.setDataValue('password', hash);
         }
-      }
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        },
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+
+  User.associate = function (models) {
+    // Associating User with Favorites
+    // When a User is deleted, also delete any associated Favorites
+    User.hasMany(models.Favorite, {
+      onDelete: "cascade"
     });
   };
-  
-  
 
-
-  // module.exports = function (sequelize, DataTypes) {
-  //   var User = sequelize.define("User", {
-  //     username: {
-  //       type: DataTypes.STRING,
-  //       allowNull: false,
-  //       unique: true,
-  //     },
-  //     email: {
-  //       type: DataTypes.STRING,
-  //       allowNull: false,
-  //       unique: true,
-  //       validate: {
-  //         isEmail: true
-  //       }
-  //     },
-  //     password: {
-  //       type: DataTypes.STRING,
-  //       allowNull: false,
-  //       length: [8, 20] // pw must be between 8 and 20 characters
-  //     }
-  //   });
-  
-  //     User.associate = function (models) {
-  //       // Associating User with Favorites
-  //       // When a User is deleted, also delete any associated Favorites
-  //       User.hasMany(models.Favorite, {
-  //         onDelete: "cascade"
-  //       });
-  //     };
-  
-  //   return User;
-  // };
+  return User;
+};
