77df737160e2c019fc2c5957c96017da703825e0
Phillip Walker <phillipw200024@gmail.com>
2023-07-18 21:34:31 -0400

    the login page shows up now

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,21 +5,21 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // const projectData = await Project.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    //const projects = projectData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('login', { 
-      projects, 
+    res.render('homepage', { 
+      // projects, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
